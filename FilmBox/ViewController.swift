eaaece5bc1508bcf1ac35cca12fab97dbd3f0617
Ahmed Ali <>
2022-06-30 16:39:58 +0200

    feat(project): add film project

---
+++ FilmBox/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FilmBox
+//
+//  Created by Ahmed Ali on 30/06/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
