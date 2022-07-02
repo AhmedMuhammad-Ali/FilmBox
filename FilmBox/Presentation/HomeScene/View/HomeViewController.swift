74d312bbc95e3db231087662f353548aa2ba85d2
Ahmed Ali <ahmedmuhammad.aliamin@gmail.com>
2022-07-02 21:23:13 +0200

    add(presentation): home view controller

---
+++ FilmBox/Presentation/HomeScene/View/HomeViewController.swift
@@ -0,0 +1,16 @@
+//
+//  HomeViewController.swift
+//  FilmBox
+//
+//  Created by Ahmed Ali on 02/07/2022.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    // MARK: - Lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
